0d5d965d693ed0d3643852cead9023458796d06f
Brayan Garcia <bgarcia.95@outlook.com>
2020-07-23 14:55:20 -0600

    Adding image field

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,8 +1,9 @@
 export const ADD_PLACE = "ADD_PLACE";
 
-export const addPlace = (title) => ({
+export const addPlace = (title, image) => ({
   type: ADD_PLACE,
   placeData: {
     title: title,
+    image: image,
   },
 });
