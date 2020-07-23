38f58ec1af2a293a4d36e5fa0ec36d21189d0284
Brayan Garcia <bgarcia.95@outlook.com>
2020-07-23 15:46:18 -0600

    Adding fetch and insert place method

--- helpers/db.js
+++ helpers/db.js
@@ -24,3 +24,49 @@ export const init = () => {
 
   return promise;
 };
+
+export const insertPlace = (title, imageUri, address, lat, lng) => {
+  const promise = new Promise((resolve, reject) => {
+    // A transaction guarantees that your query is executed as a whole and if a part fails the entire query is rolled back.
+    db.transaction((tx) => {
+      tx.executeSql(
+        `INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?,?,?,?,?)`,
+        [title, imageUri, address, lat, lng],
+        // success function
+        (_, result) => {
+          resolve(result);
+        },
+        // error function
+        // 1st arg = query executed, adding _ you signal that you don't care about it
+        (_, err) => {
+          reject(err);
+        }
+      );
+    });
+  });
+
+  return promise;
+};
+
+export const fetchPlaces = () => {
+  const promise = new Promise((resolve, reject) => {
+    // A transaction guarantees that your query is executed as a whole and if a part fails the entire query is rolled back.
+    db.transaction((tx) => {
+      tx.executeSql(
+        `SELECT * FROM places`,
+        [],
+        // success function
+        (_, result) => {
+          resolve(result);
+        },
+        // error function
+        // 1st arg = query executed, adding _ you signal that you don't care about it
+        (_, err) => {
+          reject(err);
+        }
+      );
+    });
+  });
+
+  return promise;
+};
