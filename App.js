02c483e7004464f656e3c6b5f95182580f7237e1
Brayan Garcia <bgarcia.95@outlook.com>
2020-07-23 02:25:42 -0600

    Creating store and connecting app with redux

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import PlacesNavigator from "./navigation/PlacesNavigator";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import ReduxThunk from "redux-thunk";
+import placesReducer from "./store/places-reducer";
+
+const rootReducer = combineReducers({
+  places: placesReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 const App = () => {
-  return <PlacesNavigator />;
+  return (
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
+  );
 };
 
 export default App;
