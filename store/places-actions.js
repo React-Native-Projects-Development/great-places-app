daae4dcd25216929302ac6bdeffc8f585096f354
Brayan Garcia <bgarcia.95@outlook.com>
2020-07-23 15:31:01 -0600

    Moving image from temp directory to filesystem

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,9 +1,31 @@
+import * as FileSystem from "expo-file-system";
+
 export const ADD_PLACE = "ADD_PLACE";
 
-export const addPlace = (title, image) => ({
-  type: ADD_PLACE,
-  placeData: {
-    title: title,
-    image: image,
-  },
-});
+export const addPlace = (title, image) => {
+  return async (dispatch) => {
+    const fileName = image
+      .split("/")
+      .pop(); /* with pop() I get the last segment of the splitted string */
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    // Moves from A to B
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath,
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+
+    dispatch({
+      type: ADD_PLACE,
+      placeData: {
+        title: title,
+        image: newPath,
+      },
+    });
+  };
+};
