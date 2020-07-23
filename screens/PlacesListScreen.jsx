56ea3722271a0bb465aee0a0e57faf6a1e1bfff6
Brayan Garcia <bgarcia.95@outlook.com>
2020-07-23 02:35:05 -0600

    Adding FlatList to display places

--- screens/PlacesListScreen.jsx
+++ screens/PlacesListScreen.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { View, Text, StyleSheet, Platform } from "react-native";
+import { View, StyleSheet, Platform, FlatList } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/CustomHeaderButton";
+import { useSelector } from "react-redux";
+import PlaceItem from "../components/PlaceItem";
 
 const PlacesListScreen = (props) => {
+  const places = useSelector((state) => state.places.places);
   return (
     <View style={styles.screen}>
-      <Text>Places List Screen!</Text>
+      <FlatList
+        data={places}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <PlaceItem
+            image={null}
+            title={item.title}
+            address={null}
+            onSelect={() =>
+              props.navigation.navigate("PlaceDetail", {
+                placeTitle: item.title,
+                placeId: item.id,
+              })
+            }
+          />
+        )}
+      />
     </View>
   );
 };
